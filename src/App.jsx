a3a109d24239f63275b28f37016a6c132ffa55de
clickglue <clickglue@telenet.be>
2024-10-16 22:29:59 +0200

    second iteration - item and cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import "./App.css";
+import styles from "/src/App.module.css";
 import Item from "./components/Item/Item";
 import Cart from "./components/Cart/Cart";
-import data from "../data.json"
+import data from "../data.json";
 
 function App() {
   return (
-    <>
-    {/*<Item itemData={data}></Item>*/}
-    <Cart></Cart>
-    </>
+    <main className={styles.app}>
+      <Item itemData={data}></Item>
+      <Cart></Cart>
+    </main>
   );
 }
 
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,35 +1,38 @@
-import styles from '/src/components/Cart/Cart.module.css'
-import Order from '/src/components/Order/Order'
+import styles from "/src/components/Cart/Cart.module.css";
+import Order from "/src/components/Order/Order";
 
-function Cart(){
-    return(
-        <article className={styles.cart}>
-            <header>
-                <h1>
-                Your Cart(7)
-                </h1>
-            </header>
-            <main>
-                <div className={styles.orderContainer}>
-                <Order></Order>
-                <Order></Order>
-                <Order></Order>
-                </div>
-                
-                <div>
-                    Order Total
-                    $46.50
-                </div>
-                <div>
-                    Carbon
-                </div>
-                <button>
+function Cart() {
+  return (
+    <article className={styles.cart}>
+      <header>
+        <h1>Your Cart(7)</h1>
+      </header>
+      <main>
+        <div className={styles.orderContainer}>
+          <Order></Order>
+          <Order></Order>
+          <Order></Order>
+        </div>
 
-                </button>
-            </main>
-
-        </article>
-    )
+        <div className={styles.total}>
+          <h2>Order Total</h2>
+          <p>$46.55</p>
+        </div>
+        <div className={styles.carbon}>
+          <img
+            src="/src/assets/images/icon-carbon-neutral.svg"
+            alt="Carbon Neutral"
+          />
+          <p>
+            This is a <strong>carbon-neutral</strong> delivery
+          </p>
+        </div>
+        <button>
+          <h2>Confirm Order</h2>
+        </button>
+      </main>
+    </article>
+  );
 }
 
-export default Cart
+export default Cart;
